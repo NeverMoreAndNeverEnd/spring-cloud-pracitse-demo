c3e1c6850aaf28712cbceb50a3d2bd65c9cbf225
闵继弘 <15995283349@163.com>
2019-10-23 17:55:36 +0800

    add controller

---
+++ springcloudconfigparent/config-client/src/main/java/com/minjihong/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.minjihong.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2019/10/23.
+ */
+@RestController
+public class MyController {
+
+    @Value("${foo}")
+    private String foo;
+
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+
+    }
+
+}
