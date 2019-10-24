9562ec935567fe8484bcdb368bec3a234b1f4d7f
闵继弘 <15995283349@163.com>
2019-10-24 17:44:03 +0800

    add prop2

--- springcloudconfigparent/config-client/src/main/java/com/minjihong/controller/MyController.java
+++ springcloudconfigparent/config-client/src/main/java/com/minjihong/controller/MyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    @Value("${foo}")
+    @Value("${server.port}")
     private String foo;
 
 
