346fa695787bb790e854c3d004f8d2283b1eb3ad
闵继弘 <15995283349@163.com>
2019-10-25 14:49:39 +0800

    add config

--- springcloudconfigparent/config-client/src/main/java/com/minjihong/controller/MyController.java
+++ springcloudconfigparent/config-client/src/main/java/com/minjihong/controller/MyController.java
@@ -1,7 +1,8 @@
 package com.minjihong.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    @Value("${server.port}")
+    @Value("${foo}")
     private String foo;
 
 
-    @RequestMapping(value = "/hi")
+    @GetMapping(value = "/hi")
+    @ResponseBody
     public String hi(){
         System.out.println(foo);
         return foo;
