a093a84f3e0cf1c5881101c4ad6e511b298275aa
闵继弘 <15995283349@163.com>
2020-04-12 20:25:30 +0800

    spring cloud config

---
+++ springcloudalibaba/cloud-config-center-3344/src/test/java/com/minjihong/cloud/config/center/CloudConfigCenter3344ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjihong.cloud.config.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigCenter3344ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
