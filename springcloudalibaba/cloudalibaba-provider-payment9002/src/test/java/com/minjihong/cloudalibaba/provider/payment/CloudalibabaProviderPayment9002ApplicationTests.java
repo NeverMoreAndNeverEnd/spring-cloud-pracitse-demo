6729d54782af2fc401fb5138aeb901d6f8cb48ee
闵继弘 <15995283349@163.com>
2020-05-03 19:32:40 +0800

    spring cloud alibaba nacos start

---
+++ springcloudalibaba/cloudalibaba-provider-payment9002/src/test/java/com/minjihong/cloudalibaba/provider/payment/CloudalibabaProviderPayment9002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjihong.cloudalibaba.provider.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudalibabaProviderPayment9002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
